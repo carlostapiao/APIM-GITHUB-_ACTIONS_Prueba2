22b801d6b277fd4c4445f900fba4c767b9b91445
carlostapia <carlostapiaT3csup32@gmailcom>
2026-04-07 12:14:12 -0500

    prueba 10

--- main.tf
+++ main.tf
@@ -1,53 +1,10 @@
-terraform {
-  required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
-  }
-  backend "azurerm" {
-    resource_group_name  = "rg-apppersonal-tfstate"
-    storage_account_name = "stcarlosv3state"
-    container_name       = "tfstate-apppersonal"
-    key                  = "tfstate.v7"
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# --- 1. Redes ---
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-tickets-lab"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "snet-aks"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet" "apim_subnet" {
-  name                 = "snet-apim"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-# --- 2. Seguridad APIM (NSG) ---
+# Dentro del recurso azurerm_network_security_group "apim_nsg"
 resource "azurerm_network_security_group" "apim_nsg" {
   name                = "nsg-apim"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
+  # Regla 1: Gestión de Azure (YA LA TIENES)
   security_rule {
     name                       = "AllowManagementEndpoint"
     priority                   = 100
@@ -59,134 +16,30 @@ resource "azurerm_network_security_group" "apim_nsg" {
     source_address_prefix      = "ApiManagement"
     destination_address_prefix = "VirtualNetwork"
   }
-}
 
-resource "azurerm_subnet_network_security_group_association" "apim_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.apim_subnet.id
-  network_security_group_id = azurerm_network_security_group.apim_nsg.id
-}
-
-# --- 3. AKS (Privado) ---
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                    = var.aks_name
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  dns_prefix              = "aks-tickets-priv"
-  private_cluster_enabled = true 
-
-  default_node_pool {
-    name           = "default"
-    node_count     = 1
-    vm_size        = "Standard_DC2s_v3"
-    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  # Regla 2: Tráfico Web HTTP (AÑADIR ESTA)
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "VirtualNetwork"
   }
 
-  identity { type = "SystemAssigned" }
-
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-    service_cidr      = "10.1.0.0/16" 
-    dns_service_ip    = "10.1.0.10"
+  # Regla 3: Tráfico Web HTTPS (AÑADIR ESTA)
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "VirtualNetwork"
   }
-}
-
-# --- 4. PERMISOS CRÍTICOS (Role Assignments) ---
-
-# Permiso para que AKS pueda sacar imágenes del ACR
-resource "azurerm_role_assignment" "aks_acr_new" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
-
-# SOLUCIÓN AL EXTERNAL-IP PENDING: 
-# Permiso para que el AKS pueda gestionar IPs en la VNET
-resource "azurerm_role_assignment" "aks_network" {
-  scope                = azurerm_virtual_network.vnet.id
-  role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-}
-
-# --- 5. Otros Recursos ---
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
-
-resource "azurerm_mssql_server" "sql" {
-  name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = var.db_password
-}
-
-resource "azurerm_mssql_database" "db" {
-  name      = "SupportDB"
-  server_id = azurerm_mssql_server.sql.id
-  sku_name  = "S0"
-}
-
-resource "azurerm_mssql_firewall_rule" "sql_fw" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.sql.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
-
-# --- 6. APIM v8 ---
-resource "azurerm_api_management" "apim" {
-  name                = var.apim_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = "Carlos Tapia"
-  publisher_email     = "carlos@example.com"
-  sku_name            = "Developer_1"
-
-  virtual_network_type = "External"
-  virtual_network_configuration {
-    subnet_id = azurerm_subnet.apim_subnet.id
-  }
-
-  depends_on = [azurerm_subnet_network_security_group_association.apim_nsg_assoc]
-
-  lifecycle {
-    ignore_changes = [security, hostname_configuration]
-  }
-}
-
-resource "azurerm_api_management_api" "api" {
-  name                = "tickets-api"
-  resource_group_name = azurerm_resource_group.rg.name
-  api_management_name = azurerm_api_management.apim.name
-  revision            = "1"
-  display_name        = "IT Support API"
-  path                = "tickets"
-  protocols           = ["http", "https"]
-}
-
-resource "azurerm_api_management_api_operation" "get_tickets" {
-  operation_id        = "get-tickets-api"
-  api_name            = azurerm_api_management_api.api.name
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = azurerm_resource_group.rg.name
-  display_name        = "Get All Tickets"
-  method              = "GET"
-  url_template        = "/api/tickets"
-}
-
-resource "azurerm_api_management_api_operation" "post_ticket" {
-  operation_id        = "create-ticket-api"
-  api_name            = azurerm_api_management_api.api.name
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = azurerm_resource_group.rg.name
-  display_name        = "Create Ticket"
-  method              = "POST"
-  url_template        = "/api/tickets"
 }
