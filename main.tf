82e5ac54bcb03c2e2d1912a8a6794c516ceef9fe
carlostapia <carlostapiaT3csup32@gmailcom>
2026-04-07 14:20:11 -0500

    Nueva Prueba 2

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name           = "default"
     node_count     = 1
-    vm_size        = "Standard_B2s"
+    vm_size        = "Standard_DC2s_v3 "
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
 
